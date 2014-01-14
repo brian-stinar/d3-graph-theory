7c0ebe181042b2a167f7d60ecffc5dee837f954a
Brian Stinar <Brian Stinar@192.168.0.11>
2014-01-13 20:52:35 -0700

    Fixed adjacency list generation - now we have a legit adjacency list.

--- js/graphTheory.js
+++ js/graphTheory.js
@@ -20,42 +20,44 @@ d3.graphTheory = function(nodes, edges)
     // This representation is needed for basically all following graph algoriths
     this.buildUndirectedAdjacenyList = function()
     {
-        this.adjaceyList = [];
+        this.adjaceyList = new Array(this.nodes.length);
         
-        // Initalize the array to all zeros
+        // Make our array-of-arrays
+        for (var i = 0; i < this.nodes.length; i++)
+        {
+            this.adjaceyList[i] = new Array(this.nodes.length);
+        }
+        
+        // Initalize the array of arrays to all zeros
         for (var i = 0; i < this.nodes.length; i++)
         {
             for (var j = 0; j < this.nodes.length; j++)
             {
-                this.adjaceyList.push(0);
+                this.adjaceyList[i][j] = 0;            
             }
         }
         
         // Edges is a listing of source->destination via node name
         // I need to get the indexof each element to figure out where
         // in the adjacency list things go
-        
         for (var i = 0; i < this.edges.length; i++)
         {
             var sourcePosition = this.findNodePostionInNodeList(this.edges[i]["source"])
             var targetPosition = this.findNodePostionInNodeList(this.edges[i]["target"])
-
             this.adjaceyList[sourcePosition][targetPosition] = 1;
-            this.adjaceyList[targetPosition][sourcePosition] = 1;
+            this.adjaceyList[targetPosition][sourcePosition] = 1;            
         }
     }
     
     this.printAdjacencyList = function()
     {
-        console.info(this.adjaceyList);
-        return;
-        
         var outputString = "";
+        
         for (var i = 0; i < this.nodes.length; i++)
         {
             for (var j = 0; j < this.nodes.length; j++)
             {
-                outputString+=this.adjaceyList[i][j];
+                outputString+= " " + this.adjaceyList[i][j];
             }
             console.info(outputString);
             outputString = "";
