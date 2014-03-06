3eda882fc3e6c2bfbe94ee9c3991b7e8c7ce5b33
Brian Stinar <brian@noventum.us>
2014-03-05 21:43:42 -0700

    renamed globalMethods.js to runner.js, since that is the object that has all my runner code now.

---
+++ js/runner.js
@@ -0,0 +1,50 @@
+/* 
+ * Runners - objects which run everything.
+ * 
+    Brian J. Stinar
+    Noventum Custom Software Development 
+    505-750-1169  - Call me if you want to talk or have questions. 
+    I like that more than email. 
+ */
+
+
+
+function Runner(data)
+{
+    var m_data = data;
+
+    // This different members have a lot of similarities. Maybe I can 
+    // make one private method that has all the similar parts, which is called 
+    // first by both?
+    this.runSubgraphDetection = function()
+    {
+        var graphTheory = d3.graphTheory(m_data["nodes"], m_data["edges"]);
+        var disjointSets = graphTheory.detectDisjointSubgraphs(); 
+
+        for (var setIndex in disjointSets)
+        {
+            console.log("setIndex = " + setIndex);
+            for (var nodeIndex in disjointSets[setIndex])
+            {
+                console.log("nodeIndex = " + nodeIndex);
+                disjointSets[setIndex][nodeIndex]["type"] = setIndex;
+            }
+        }
+
+        buildGraph(m_data);
+    };
+
+
+    this.runDijkstras = function()
+    {
+        var graphTheory = d3.graphTheory(m_data["nodes"], m_data["edges"]);
+        var source = m_data["nodes"][2];
+        source["type"] = "source";
+        graphTheory.dijkstras(source);
+        console.log("source['name'] = ");
+        console.log(source['name']);
+
+        var graph = buildGraph(m_data);
+        graph.buildTextFromDistance();
+    }
+}
